64ef298e749fcfa3624df9ba5cf2bb0788a56483
Larpot <jts1535@naver.com>
2021-03-20 14:55:29 +0900

    init code - show hello world

---
+++ src/main/java/com/aboutme/springwebservice/SpringWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.aboutme.springwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebserviceApplication.class, args);
+    }
+
+}
